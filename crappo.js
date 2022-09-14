cf37054e0eaca98aef1bb1a379a5eab16aed17d5
Kachii <kachikaduru@gmail.com>
2022-09-14 06:01:37 +0100

    mobile version update

---
+++ crappo.js
@@ -0,0 +1,24 @@
+// "use strict";
+
+// const menu = document.getElementById("mobileMenu");
+// const exit = document.getElementById("exit");
+// const nav = document.querySelector("nav");
+
+// const toggleNav = function () {
+//   if (nav.classList.contains("open")) {
+//     nav.classList.remove("open");
+//     menu.style.width = "2.7rem";
+//     exit.style.width = "0";
+//     menu.style.overflow = "auto";
+//     menu.style.overflow = "hidden";
+//   } else {
+//     nav.classList.add("open");
+//     menu.style.width = "0";
+//     exit.style.width = "4rem";
+//     menu.style.overflow = "hidden";
+//     exit.style.overflow = "auto";
+//   }
+// };
+
+// menu.addEventListener("click", toggleNav);
+// exit.addEventListener("click", toggleNav);
